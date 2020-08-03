a14e76ce2bd1243bd117a0c67d0e80ec42059022
Vikram Singh <51959049+vikysingh@users.noreply.github.com>
2020-08-03 15:46:28 +0200

    Add files via upload

---
+++ build/precache-manifest.0f72625f0e1936606d9c29f9fecc33d1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "776a2c4d6047f4fc11ecbceed72f0015",
+    "url": "/index.html"
+  },
+  {
+    "revision": "31a8fd88e16269695e6c",
+    "url": "/static/css/main.3158bd77.chunk.css"
+  },
+  {
+    "revision": "0e6d4800fc2002ba1978",
+    "url": "/static/js/2.0ea6bf53.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.0ea6bf53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31a8fd88e16269695e6c",
+    "url": "/static/js/main.4443d45d.chunk.js"
+  },
+  {
+    "revision": "cb968986a79366378a6a",
+    "url": "/static/js/runtime-main.8fdc2ba7.js"
+  },
+  {
+    "revision": "547853925280c06373799147cec90c1c",
+    "url": "/static/media/card_img.54785392.jpeg"
+  },
+  {
+    "revision": "bf0fb362abca26e82593c84f523620d4",
+    "url": "/static/media/illustration.bf0fb362.jpg"
+  }
+]);
